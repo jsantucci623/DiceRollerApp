64c537591d10b1e76e7d0de33bbd9ed82543a223
Jeff Santucci <jeffrey.santucci@gmail.com>
2023-01-23 13:55:47 -0500

    DiceRoller Initial Commit

---
+++ DiceRoller/DiceRollerMethods.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Pipes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiceRoller
+{
+    internal class DiceRollerMethods
+    {
+        public static int DieNumber(int numSides)
+        {
+            Random rand = new Random();
+            int num;
+            do
+            {
+                num = rand.Next(numSides);
+
+            } while (num == 0);
+
+            return num;
+        }
+        public static string DiceResult(int totalDie)
+        {
+            string answer;
+            int num;
+            num = totalDie;
+            if (num == 2)
+            {
+                answer = "Snake Eyes";
+            }
+            else if (num == 3)
+            {
+                answer = "Ace Deuce";
+            }
+            else if (num == 12)
+            {
+                answer = "Box Cars";
+            }
+            else
+                answer = "";
+
+            return answer;
+        }
+        public static int DiceTotal(int dieTot1, int dieTot2)
+        {
+            int totalDie = dieTot1 + dieTot2;
+            return totalDie;
+        }
+        public static string WinCraps(int dieTot1, int dieTot2)
+        {
+            string answer;
+            int totalDie = DiceTotal(dieTot1, dieTot2);
+
+            if (totalDie == 7 || totalDie == 11)
+            {
+                answer = "Winner";
+            }
+            else if (totalDie == 2 || totalDie == 3 || totalDie == 12)
+            {
+                answer = "Craps";
+            }
+            else
+                answer = "";
+
+            return answer;
+        }  
+
+    }
+}
---
+++ DiceRoller/Program.cs
@@ -0,0 +1,60 @@
+﻿// Dice Roller
+// Jeff Santucci
+// 01/19/23
+
+using DiceRoller;
+
+bool isValidNum;
+bool tryAgain = true;
+do
+{
+    Console.WriteLine("Enter the number of sides for a pair of dice? ");
+    string sides = Console.ReadLine();
+    isValidNum = int.TryParse(sides, out int numSides);
+
+    do
+    {
+        if (!isValidNum)
+        {
+            Console.WriteLine(sides + " Is not a valid number! Try Again (y/n)? ");
+            string answer = Console.ReadLine();
+            if (answer != "y")
+            {
+                tryAgain = false;
+            }
+        }
+        else
+        {
+            Console.WriteLine("Roll the dice!");
+            int dieNum1 = DiceRollerMethods.DieNumber(numSides);
+            Console.WriteLine(dieNum1 + " first die");
+            int dieNum2 = DiceRollerMethods.DieNumber(numSides);
+            Console.WriteLine(dieNum2 + " Second die");
+            if (int.Parse(sides) == 6)
+            {
+                int totalDie = DiceRollerMethods.DiceTotal(dieNum1, dieNum2);
+                string diceResult = DiceRollerMethods.DiceResult(totalDie);
+                Console.WriteLine(diceResult);
+                string diceWinner = DiceRollerMethods.WinCraps(dieNum1, dieNum2);
+                Console.WriteLine(diceWinner);
+            }
+            tryAgain = false;
+        }
+    } while (tryAgain);
+
+    Console.WriteLine("Would you like to roll the dice again? (y/n)? ");
+    string anotherAnswer = Console.ReadLine();
+    if (anotherAnswer != "y")
+    {
+        tryAgain = false;   
+    }
+    else
+    {
+        tryAgain = true;
+    }
+} while (tryAgain);
+
+
+
+
+
